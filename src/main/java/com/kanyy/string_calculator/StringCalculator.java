a81cc33b967f72baa4f508d7ef77328ba1bbe713
Kanhaiya Singh <kanhaiya.singh@wolterskluwer.com>
2020-05-29 15:09:49 +0530

    Implemented add method
    Implemented add method for adding string up to 3 numbers and few 1 test cases "addUptoTwoNumbersWithEmptyString" with few scenarios.

--- src/main/java/com/kanyy/string_calculator/StringCalculator.java
+++ src/main/java/com/kanyy/string_calculator/StringCalculator.java
@@ -1,5 +1,15 @@
 package com.kanyy.string_calculator;
 
 public class StringCalculator {
+  public int add(String numbers) {
+    int sum = 0;
+    String[] nums = numbers.split(",");
+    for (int i = 0; i < nums.length && i < 3; i++) {
+      if (!nums[i].equals("")) {
+        sum += Integer.parseInt(nums[i]);
+      }
+    }
+    return sum;
+  }
 
 }
--- src/test/java/com/kanyy/string_calculator/StringCalculatorTest.java
+++ src/test/java/com/kanyy/string_calculator/StringCalculatorTest.java
@@ -1,5 +1,20 @@
 package com.kanyy.string_calculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringCalculatorTest {
 
+  private StringCalculator sc = new StringCalculator();
+
+  @Test
+  public void addUptoTwoNumbersWithEmptyString() {
+    int[] expecteds = new int[] {0, 5, 3, 3};
+    String[] inputs = new String[] {"", "5", ",3,", "1,2"};
+    int[] actuals = new int[inputs.length];
+    for (int i = 0; i < inputs.length; i++) {
+      actuals[i] = sc.add(inputs[i]);
+    }
+    Assert.assertArrayEquals(expecteds, actuals);
+  }
 }
