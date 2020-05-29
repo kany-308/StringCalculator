cc239dca6c393f1d6bc58dc01a329df68bc625a2
Kanhaiya Singh <kanhaiya.singh@wolterskluwer.com>
2020-05-29 15:23:46 +0530

    Updated add method
    Updated  add method to support new line as a delimeter and 1 test cases "addNumbersWithSupportOfNewLineDelimeter" with few scenarios.

--- src/main/java/com/kanyy/string_calculator/StringCalculator.java
+++ src/main/java/com/kanyy/string_calculator/StringCalculator.java
@@ -3,7 +3,7 @@ package com.kanyy.string_calculator;
 public class StringCalculator {
   public int add(String numbers) {
     int sum = 0;
-    String[] nums = numbers.split(",");
+    String[] nums = numbers.split(",|\n");
     for (int i = 0; i < nums.length; i++) {
       if (!nums[i].equals("")) {
         sum += Integer.parseInt(nums[i]);
